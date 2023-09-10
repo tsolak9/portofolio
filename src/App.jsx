8728eb703439c620cd19d33d381923ba77afd5c2
tsolak <tsolakidis.anatolios@gmail.com>
2023-09-10 14:56:01 +0300

    updated home page and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/header'
+import Footer from './components/footer'
 
 function App() {
 
@@ -7,12 +8,15 @@ function App() {
     <>
     <Header />
     <main>
-      <div className="container mx-auto px-4">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      </div>
+      <section className='container mx-auto px-4'>
+        <h1>👋 Hello There.</h1>
+        <p>My name is Anatolios. I’m a Junior Front-End Developer!
+
+Currently immersed in the Codecademy career path for Front-End Engineering, I've already honed my skills in crafting captivating web experiences using HTML and CSS. As I eagerly delve into the world of JavaScript, I'm excited to breathe life into my creations with interactivity and functionality. Beyond the screen and lines of code, you'll find me embracing the joys of exercise during my free moments.</p>
+    
+      </section>
     </main>
+    <Footer />
      
     </>
   )
