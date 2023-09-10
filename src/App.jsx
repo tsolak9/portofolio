cbd7494879ac240dc16636927bc3107f6fae739c
tsolak <tsolakidis.anatolios@gmail.com>
2023-09-10 13:04:44 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Header from './components/header'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <main>
+      <div className="container mx-auto">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      </div>
+    </main>
+     
+    </>
+  )
+}
+
+export default App
+
+
---
+++ src/components/header.jsx
@@ -0,0 +1,20 @@
+
+
+function Header() {
+  return (
+    <header>
+        <div className="container mx-auto flex justify-between">
+        <img src="#" alt="Tsolakidis Anatolios" />
+        <nav>
+            <ul  className="flex">
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
